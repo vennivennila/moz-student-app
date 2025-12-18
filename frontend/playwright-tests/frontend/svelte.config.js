6b36366391e701d8db5ff92b932c61de90eaadad
VENNILA <134380226+vennivennila@users.noreply.github.com>
2025-12-18 11:21:03 +0530

    Delete frontend/playwright-tests/frontend directory

--- frontend/playwright-tests/frontend/svelte.config.js
+++
@@ -1,9 +0,0 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
-
-export default {
-  preprocess: preprocess(),
-  kit: {
-    adapter: adapter()
-  }
-};
